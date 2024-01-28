8f46474eed9dbf7af3e655d6381d6789c9a975ff
azure-architect <vscoder.brad@gmail.com>
2024-01-28 11:56:26 +0800

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.{html,js,php,twig}', // Scan for HTML, JS, PHP, and Twig files in the views directory
+    // './*.php', // Scan all PHP files in the root directory
+    // Add other paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
